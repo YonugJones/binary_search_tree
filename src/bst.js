14c2fe7b86260ae7085cc926539da933a87eceda
YonugJones <peterckerfoot@gmail.com>
2024-02-20 11:00:13 -0600

    Created class constructor for node and tree added build tree method to tree class.

--- src/bst.js
+++ src/bst.js
@@ -1,7 +1,21 @@
-export default class Node {
-    constructor(data) {
-        this.data = data;
-        this.left = null;
-        this.right = null;
+import Node from "./nodeClass"
+
+export default class Tree {
+    constructor(array) {
+        this.root = this.buildTree(array)
     }
+
+
+    buildTree(array, start = 0, end = array.length - 1) {
+        const sortedUniqueArray = Array.from(new Set(array)).sort((a, b) => a - b);
+
+        if (start > end) return null;
+        const mid = Math.floor((start + end) / 2);
+        const node = new Node(sortedUniqueArray[mid]);
+        node.left = this.buildTree(sortedUniqueArray, start, mid - 1);
+        node.right = this.buildTree(sortedUniqueArray, mid + 1, end);
+
+        return node;
+    }
+
 }
--- src/index.js
+++ src/index.js
@@ -1,4 +1,2 @@
-import Node from "./bst";
 
-const nodeOne = new Node([1, 2, 3]);
-console.log(nodeOne);
+
