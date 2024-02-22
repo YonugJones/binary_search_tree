41c30e7c1719f4b46c5250904bd7d103e9887b23
YonugJones <peterckerfoot@gmail.com>
2024-02-22 09:31:29 -0600

    added a sort method and decoupled logic from build tree method.

--- dist/main.js
+++ dist/main.js
@@ -7,26 +7,100 @@
  * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
  */
 /******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
 /******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/bst.js":
+/*!********************!*\
+  !*** ./src/bst.js ***!
+  \********************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (/* binding */ Tree)\n/* harmony export */ });\n/* harmony import */ var _nodeClass__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./nodeClass */ \"./src/nodeClass.js\");\n\nclass Tree {\n  constructor(array) {\n    const sortedUniqueArray = Tree.sortAndRemoveDuplicates(array);\n    this.root = this.buildTree(sortedUniqueArray);\n  }\n  static sortAndRemoveDuplicates(array) {\n    return Array.from(new Set(array)).sort((a, b) => a - b);\n  }\n  buildTree(array) {\n    let start = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : 0;\n    let end = arguments.length > 2 && arguments[2] !== undefined ? arguments[2] : array.length - 1;\n    if (start > end) return null;\n    const mid = Math.floor((start + end) / 2);\n    const root = new _nodeClass__WEBPACK_IMPORTED_MODULE_0__[\"default\"](array[mid]);\n    root.left = this.buildTree(array, start, mid - 1);\n    root.right = this.buildTree(array, mid + 1, end);\n    return root;\n  }\n}\n\n//# sourceURL=webpack://binary_search_tree/./src/bst.js?");
+
+/***/ }),
 
 /***/ "./src/index.js":
 /*!**********************!*\
   !*** ./src/index.js ***!
   \**********************/
-/***/ (() => {
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
-eval("\n\n//# sourceURL=webpack://binary_search_tree/./src/index.js?");
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _bst__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./bst */ \"./src/bst.js\");\n\nconst array = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324];\nconst prettyPrint = function (node) {\n  let prefix = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : \"\";\n  let isLeft = arguments.length > 2 && arguments[2] !== undefined ? arguments[2] : true;\n  if (node === null) {\n    return;\n  }\n  if (node.right !== null) {\n    prettyPrint(node.right, `${prefix}${isLeft ? \"│   \" : \"    \"}`, false);\n  }\n  console.log(`${prefix}${isLeft ? \"└── \" : \"┌── \"}${node.data}`);\n  if (node.left !== null) {\n    prettyPrint(node.left, `${prefix}${isLeft ? \"    \" : \"│   \"}`, true);\n  }\n};\nconst tree = new _bst__WEBPACK_IMPORTED_MODULE_0__[\"default\"](array);\nprettyPrint(tree.root);\nconsole.log(tree.root);\n\n//# sourceURL=webpack://binary_search_tree/./src/index.js?");
+
+/***/ }),
+
+/***/ "./src/nodeClass.js":
+/*!**************************!*\
+  !*** ./src/nodeClass.js ***!
+  \**************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (/* binding */ Node)\n/* harmony export */ });\nclass Node {\n  constructor(data) {\n    this.data = data;\n    this.left = null;\n    this.right = null;\n  }\n}\n\n//# sourceURL=webpack://binary_search_tree/./src/nodeClass.js?");
 
 /***/ })
 
 /******/ 	});
 /************************************************************************/
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
+/******/ 		if (cachedModule !== undefined) {
+/******/ 			return cachedModule.exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	
+/************************************************************************/
+/******/ 	/* webpack/runtime/define property getters */
+/******/ 	(() => {
+/******/ 		// define getter functions for harmony exports
+/******/ 		__webpack_require__.d = (exports, definition) => {
+/******/ 			for(var key in definition) {
+/******/ 				if(__webpack_require__.o(definition, key) && !__webpack_require__.o(exports, key)) {
+/******/ 					Object.defineProperty(exports, key, { enumerable: true, get: definition[key] });
+/******/ 				}
+/******/ 			}
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/hasOwnProperty shorthand */
+/******/ 	(() => {
+/******/ 		__webpack_require__.o = (obj, prop) => (Object.prototype.hasOwnProperty.call(obj, prop))
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/make namespace object */
+/******/ 	(() => {
+/******/ 		// define __esModule on exports
+/******/ 		__webpack_require__.r = (exports) => {
+/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 			}
+/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/************************************************************************/
 /******/ 	
 /******/ 	// startup
 /******/ 	// Load entry module and return exports
 /******/ 	// This entry module can't be inlined because the eval devtool is used.
-/******/ 	var __webpack_exports__ = {};
-/******/ 	__webpack_modules__["./src/index.js"]();
+/******/ 	var __webpack_exports__ = __webpack_require__("./src/index.js");
 /******/ 	
 /******/ })()
 ;
--- src/bst.js
+++ src/bst.js
@@ -1,21 +1,23 @@
-import Node from "./nodeClass"
+import Node from "./nodeClass";
 
-export default class Tree {
+export default class Tree{
     constructor(array) {
-        this.root = this.buildTree(array)
+        const sortedUniqueArray = Tree.sortAndRemoveDuplicates(array);
+        this.root = this.buildTree(sortedUniqueArray);
     }
 
+    static sortAndRemoveDuplicates(array) {
+        return Array.from(new Set(array)).sort((a, b) => a - b);
+    }
 
     buildTree(array, start = 0, end = array.length - 1) {
-        const sortedUniqueArray = Array.from(new Set(array)).sort((a, b) => a - b);
-
         if (start > end) return null;
+
         const mid = Math.floor((start + end) / 2);
-        const node = new Node(sortedUniqueArray[mid]);
-        node.left = this.buildTree(sortedUniqueArray, start, mid - 1);
-        node.right = this.buildTree(sortedUniqueArray, mid + 1, end);
+        const root = new Node(array[mid]);
+        root.left = this.buildTree(array, start, mid - 1);
+        root.right = this.buildTree(array, mid + 1, end);
 
-        return node;
+        return root;
     }
-
 }
